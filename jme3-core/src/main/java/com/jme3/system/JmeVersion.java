4f2933dd88dcca7cbe328aa21aae3aa0bf1da198
Hec-P <hectorpriori@gmail.com>
2015-08-10 18:38:58 -0300

    Remove JmeVersion

---
+++ jme3-core/src/main/java/com/jme3/system/JmeVersion.java
@@ -0,0 +1,14 @@
+
+package com.jme3.system;
+
+/**
+ * THIS IS AN AUTO-GENERATED FILE..
+ * DO NOT MODIFY!
+ */
+public class JmeVersion {
+    public static final String BUILD_DATE = "2015-07-12";
+    public static final String BRANCH_NAME = "master";
+    public static final String GIT_HASH = "95d5fdf9c5b45f1bbd4d52e8e2a17f21ad03faa5";
+    public static final String GIT_SHORT_HASH = "95d5fdf";
+    public static final String FULL_NAME = "jMonkeyEngine 3.1.0 (snapshot-github)";
+}
